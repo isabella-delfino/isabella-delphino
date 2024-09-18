043c279e3f5f54cce87fcf851c2cff0236e14147
isabella-delfino <blt.isabella.delphino@gmail.com>
2024-09-17 21:53:29 -0700

    add form

--- src/components/UI/organisms/Form/Contact/index.vue
+++ src/components/UI/organisms/Form/Contact/index.vue
@@ -1,51 +1,28 @@
 <template>
-  <el-form
-    ref="ruleFormRef"
-    style="max-width: 600px"
-    :model="ruleForm"
-    status-icon
-    :rules="rules"
-  >
+  <el-form ref="ruleFormRef" style="max-width: 600px" :model="ruleForm" status-icon :rules="rules">
 
     <el-form-item prop="name">
-      <el-input 
-        :placeholder="$t('pages.contact.form.name')" 
-        v-model="ruleForm.name" 
-        autocomplete="off" 
-        class="custom-input"
-      />
+      <el-input :placeholder="$t('pages.contact.form.name')" v-model="ruleForm.name" autocomplete="off"
+        class="custom-input" />
     </el-form-item>
-    
+
     <el-form-item prop="email">
-      <el-input
-        :placeholder="$t('pages.contact.form.email')"
-        v-model="ruleForm.email"
-        type="email"
-        class="custom-input"
-      />
+      <el-input :placeholder="$t('pages.contact.form.email')" v-model="ruleForm.email" type="email"
+        class="custom-input" />
     </el-form-item>
-    
+
     <el-form-item prop="subject">
-      <el-input 
-        :placeholder="$t('pages.contact.form.subject')" 
-        v-model.number="ruleForm.subject" 
-        class="custom-input"
-      />
+      <el-input :placeholder="$t('pages.contact.form.subject')" v-model.number="ruleForm.subject"
+        class="custom-input" />
     </el-form-item>
 
     <el-form-item prop="message">
-      <el-input 
-        :placeholder="$t('pages.contact.form.message')" 
-        v-model.number="ruleForm.message" 
-        type="textarea"
-        :rows="10" 
-        cols="50"
-        class="custom-input"
-      />
+      <el-input :placeholder="$t('pages.contact.form.message')" v-model.number="ruleForm.message" type="textarea"
+        :rows="10" cols="50" class="custom-input" />
     </el-form-item>
 
     <el-form-item>
-      <el-button type="primary" @click="submitForm(ruleFormRef)" round>
+      <el-button :disabled="isDisabled" type="primary" @click="submitForm(ruleFormRef)" round>
         {{ $t('pages.contact.form.submit') }}
       </el-button>
     </el-form-item>
@@ -56,8 +33,10 @@
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
+import { ElNotification } from 'element-plus'
 
 const ruleFormRef = ref<FormInstance>()
+const isDisabled = ref(false)
 
 //@ts-ignore
 const requiredField = (fieldName: string) => (rule: any, value: any, callback: any) => {
@@ -97,12 +76,33 @@ const rules = reactive<FormRules<typeof ruleForm>>({
   message: [{ validator: requiredField('message'), trigger: 'blur' }]
 })
 
-const submitForm = (formEl: FormInstance | undefined) => {
+const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
-  formEl.validate((valid) => {
+  formEl.validate(async (valid) => {
     if (valid) {
-      console.log('submit!')
-      console.log(ruleForm, "form")
+      const response = await fetch('https://web-backend-69lw.onrender.com/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ruleForm)
+      }).then(res => res.json()).catch(err => console.log(err))
+
+      if (response.success) {
+        ElNotification({
+          title: 'Success',
+          message: 'Correo enviado con exito',
+          type: 'success',
+        })
+      } else {
+        ElNotification({
+          title: 'Error',
+          message: 'El correo no pudo ser enviado, por favor llamenos directamente',
+          type: 'error',
+        })
+      }
+
+      isDisabled.value = true;
     } else {
       console.log('error submit!')
     }
@@ -125,11 +125,13 @@ const submitForm = (formEl: FormInstance | undefined) => {
 
 .custom-input :deep(.el-input__inner::placeholder) {
   color: #000000;
-  opacity: 1; /* Ensure the placeholder color is fully opaque */
+  opacity: 1;
+  /* Ensure the placeholder color is fully opaque */
 }
 
 .custom-input :deep(.el-textarea__inner::placeholder) {
   color: #000000;
-  opacity: 1; /* Ensure the placeholder color is fully opaque */
+  opacity: 1;
+  /* Ensure the placeholder color is fully opaque */
 }
 </style>
